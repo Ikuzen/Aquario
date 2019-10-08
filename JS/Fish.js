effce5775d3809459a7ef8ff5bac75a1e6051c55
Yannis <yannis.samba@gmail.com>
2019-10-08 14:13:22 +0200

    add boundaries, and "bounce" from the corner effect.

--- JS/Fish.js
+++ JS/Fish.js
@@ -6,22 +6,27 @@ class Fish {
     this.w = w;
     this.h = h;
     this.color = color;
-    this.startingAngle = this.randomAngle()
+    this.angle = this.randomAngle()
     this.direction = {
-      x: Math.sin(this.startingAngle),
-      y: -Math.cos(this.startingAngle),
+      x: Math.sin(this.angle),
+      y: -Math.cos(this.angle),
     }
   }
   newRandomDirection() {
     let angle = this.randomAngle();
     this.direction.y = -Math.cos(angle);
     this.direction.x = Math.sin(angle);
-
+  }
+  reverseDirection(axis){
+    if(axis === "x"){
+      this.direction.x = - this.direction.x
+    }else{
+      this.direction.y = - this.direction.y
+    }
   }
   move() {
     this.x += this.direction.x;
     this.y += this.direction.y;
-    console.log(this.direction)
   }
   randomAngle(){
     let angle = Math.floor(Math.random()*360)*Math.PI/180;
--- JS/aquariumManager.js
+++ JS/aquariumManager.js
@@ -9,14 +9,21 @@ class aquariumManager {
     this.playScreen = document.getElementById("aquarium-screen");
     this.image = this.playScreen.getContext("2d");
     this.tempMousePos;
+    this.bounds = this.playScreen.getBoundingClientRect();
     this.getMousePosition = (event) =>{
       this.tempMousePos = {
         x:event.clientX,
         y:event.clientY
       }
-      
+      console.log(this.tempMousePos)
     }
-    this.bounds = this.playScreen.getBoundingClientRect();
+    this.aquariumBoundary = {
+      aquariumBoundaryLeftX:this.bounds.x-100,
+      aquariumBoundaryRightX:870-this.bounds.x,
+      aquariumBoundaryTopY:this.bounds.y-100,
+      aquariumBoundaryBottomY:700-this.bounds.y,
+    }
+ 
   }
   ///
   // generation functions
@@ -27,22 +34,25 @@ class aquariumManager {
     console.log("loaded background")
   }
   generateFish() {
-    this.fishArray.push(new Fish(this.tempMousePos.x-this.bounds.x-7.5, this.tempMousePos.y-this.bounds.y-7.5, 15, 15, this.colors["red"]))
-    console.log(this.fishArray)
+    if(this.tempMousePos.y <= 700){ // generate fish only in the upper side of the canvas
+      this.fishArray.push(new Fish(this.tempMousePos.x-this.bounds.x-7.5, this.tempMousePos.y-this.bounds.y-7.5, 15, 15, this.colors["red"]))
+      console.log(this.fishArray)
+    }
   }
 
   ///
   /// Display functions
   ///
   drawAllFishes(){ // as rectangle shapes
-    for(let i=0;i<this.fishArray.length;i++){
-      this.image.fillStyle = this.fishArray[i].colors;
+    for(let fish of this.fishArray){
+      this.checkAquariumCollision(fish)
+      this.image.fillStyle = fish.colors;
       this.image.fillRect(
-        this.fishArray[i].x,
-        this.fishArray[i].y,
-        this.fishArray[i].w,
-        this.fishArray[i].h)
-        this.fishArray[i].move(); // update next position of fishes
+        fish.x,
+        fish.y,
+        fish.w,
+        fish.h)
+        fish.move(); // update next position of fishes
     }
   }
   clearCanvas(){
@@ -55,5 +65,18 @@ class aquariumManager {
   destroyFishes(){
     this.fishArray= [];
     console.log("DIE")
+  }
+
+  ///
+  //specific functions
+  ///
+  
+  checkAquariumCollision(fish){
+    if(fish.x <= this.aquariumBoundary.aquariumBoundaryLeftX || fish.x >= this.aquariumBoundary.aquariumBoundaryRightX ){
+      fish.reverseDirection("x") 
+    }
+    else if(fish.y <= this.aquariumBoundary.aquariumBoundaryTopY || fish.y >= this.aquariumBoundary.aquariumBoundaryBottomY){
+      fish.reverseDirection("y") 
+    }
   }
 }
